08733de6d5226efa5b5902dd3ded31208a4d3e6d
DESKTOP-QCSP4RS\m_mam <m_mammadli@hotmail.com>
2021-11-29 18:52:52 -0500

    Added basic code for frame,name,and logo

--- SmartTrader.py
+++ SmartTrader.py
@@ -1,1 +1,82 @@
 import tkinter as tk
+import PyPDF2
+from PIL import Image,ImageTk
+
+class Page(tk.Frame):
+    def __init__(self, *args, **kwargs):
+        tk.Frame.__init__(self, *args, **kwargs)
+    def show(self):
+        self.lift()
+
+class Page1(Page):
+   def __init__(self, *args, **kwargs):
+       Page.__init__(self, *args, **kwargs)
+       label = tk.Label(self, text="This is page 1")
+       label.pack(side="top", fill="both", expand=True)
+
+class Page2(Page):
+   def __init__(self, *args, **kwargs):
+       Page.__init__(self, *args, **kwargs)
+       label = tk.Label(self, text="This is page 2")
+       label.pack(side="top", fill="both", expand=True)
+
+class Page3(Page):
+   def __init__(self, *args, **kwargs):
+       Page.__init__(self, *args, **kwargs)
+       label = tk.Label(self, text="This is page 3")
+       label.pack(side="top", fill="both", expand=True)
+
+class MainView(tk.Frame):
+    def __init__(self, *args, **kwargs):
+        tk.Frame.__init__(self, *args, **kwargs)
+        p1 = Page1(self)
+        p2 = Page2(self)
+        p3 = Page3(self)
+
+        buttonframe = tk.Frame(self)
+        container = tk.Frame(self)
+        buttonframe.pack(side="top", fill="x", expand=False)
+        container.pack(side="top", fill="both", expand=True)
+
+        p1.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+        p2.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+        p3.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+
+        b1 = tk.Button(buttonframe, text="Page 1", command=p1.show)
+        b2 = tk.Button(buttonframe, text="Page 2", command=p2.show)
+        b3 = tk.Button(buttonframe, text="Page 3", command=p3.show)
+
+        b1.pack(side="left")
+        b2.pack(side="left")
+        b3.pack(side="left")
+
+        p1.show()
+        
+
+root = tk.Tk()
+root.title('SmartTrader 1.0')
+root.iconbitmap('favicon.ico')
+
+canvas = tk.Canvas(root,width=600,height=300)
+canvas.grid(columnspan=3,rowspan=3)
+
+#logo
+file = "coinbase-new4201.jpg"
+logo = Image.open(file)
+logo = ImageTk.PhotoImage(logo)
+logo_label = tk.Label(image=logo)
+logo_label.image_names = logo
+logo_label.grid(column=1,row=0)
+
+#instructions
+instructions = tk.Label(root, text='SmartTrader 1.0')
+instructions.grid(columnspan=3,column=0,row=1)
+
+#browse button
+browse_text = tk.StringVar()
+browse_bin = tk.Button(root, textvariable=browse_text, bg="#20bebe", height=2, width=15)
+browse_text.set("Ready for Profits?")
+browse_bin.grid(column=1,row=2)
+    
+
+root.mainloop()
