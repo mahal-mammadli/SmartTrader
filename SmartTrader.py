6a39159f3dbd2dca93e72b36fc91717db49343cc
DESKTOP-QCSP4RS\m_mam <m_mammadli@hotmail.com>
2021-11-29 23:24:23 -0500

    Added exit program button

--- SmartTrader.py
+++ SmartTrader.py
@@ -1,3 +1,4 @@
+import time as time
 import tkinter as tk
 from tkinter.constants import COMMAND
 import PyPDF2
@@ -7,8 +8,14 @@ root = tk.Tk()
 root.title('SmartTrader 1.0')
 root.iconbitmap('favicon.ico')
 
+def window():
+    tk.Toplevel()
+    
+
 class Application:
     def __init__(self, master):
+
+        #Start Frame
         myFrame = tk.Frame(master)
 
         self.canvas = tk.Canvas(master,width=600,height=300)
@@ -22,16 +29,18 @@ class Application:
         self.logo_label.image_names = self.logo
         self.logo_label.grid(column=1,row=0)
         
-        #instructions
-        self.instructions = tk.Label(master, text='SmartTrader 1.0')
-        self.instructions.grid(columnspan=3,column=0,row=1)
+        #Enter program button
+        self.button_enter = tk.Button(master, text="Start Program", command=lambda:[self.funcA(), self.funcB()])
+        self.button_enter.grid(column=1,row=1)
 
-        #browse button
-        self.browse_text = tk.StringVar()
-        self.browse_bin = tk.Button(master, textvariable=self.browse_text, bg="#20bebe", height=2, width=15)
-        self.browse_text.set("Ready for Profits?")
-        self.browse_bin.grid(column=1,row=2)
-        
+        #Exit program button
+        self.button_quit = tk.Button(master, text="Exit Program", command=master.quit)
+        self.button_quit.grid(column=1,row=2)
+
+    def funcA(self):
+        window()
+    def funcB(self):
+        window()
          
 # Run application
 app = Application(root)
