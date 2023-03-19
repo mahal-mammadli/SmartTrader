bde836f5f5d550769ffdef6aa94143ae1e6e4bdd
DESKTOP-QCSP4RS\m_mam <m_mammadli@hotmail.com>
2023-03-19 18:02:36 -0400

    improved code

--- Source/TradingView.py
+++ Source/TradingView.py
@@ -1,31 +1,28 @@
 import sys
 from tradingview_ta import TA_Handler, Interval
 import customtkinter
-
+from yahoo_fin.stock_info import get_day_most_active
 from StartPage import StdoutRedirector
 
 def get_tradingview_data(symbol):
     handler = TA_Handler(
-    symbol=symbol,
-    screener="america",
-    exchange="NASDAQ",
-    interval=Interval.INTERVAL_1_MINUTE
+        symbol=symbol,
+        screener="america",
+        exchange="NASDAQ",
+        interval=Interval.INTERVAL_1_MINUTE
     )
-    analysis = handler.get_analysis()
+    return handler.get_analysis()
 
-    current_price = analysis.summary.get("RECOMMENDATION")
-    rsi = analysis.indicators.get("RSI")
-
-    return current_price, rsi
-    
 class TradingView(customtkinter.CTkFrame):
     def __init__(self, parent, controller):
-        customtkinter.CTkFrame.__init__(self, parent)
+        super().__init__(parent)
         self.controller = controller
 
         def submit():
-            current_price, rsi = get_tradingview_data(symbol_dropdown.get())
-            print(current_price,rsi)
+            analysis = get_tradingview_data(symbol_dropdown.get())
+            current_price = analysis.summary.get("RECOMMENDATION")
+            rsi = analysis.indicators.get("RSI")
+            print(current_price, rsi)
 
         # Return button
         button = customtkinter.CTkButton(self, text="Return",
@@ -36,7 +33,11 @@ class TradingView(customtkinter.CTkFrame):
         symbol_label = customtkinter.CTkLabel(self, text="Symbol:")
         symbol_label.pack()
 
-        symbol_dropdown = customtkinter.CTkOptionMenu(self, values=["AAPL", "TSLA", "MSFT"])
+        # Get the list of stocks sorted by market cap
+        top_stocks = get_day_most_active()
+        popular_stocks = list(top_stocks[:10].Symbol)
+
+        symbol_dropdown = customtkinter.CTkOptionMenu(self, values=popular_stocks)
         symbol_dropdown.pack()
 
         submit_button = customtkinter.CTkButton(self, text="Submit", command=submit)
@@ -47,6 +48,4 @@ class TradingView(customtkinter.CTkFrame):
         self.console_text.pack(pady=50, side="bottom", fill="both", expand=True)  # stretch to the sides of the window
 
         # redirect console output to text widget
-        sys.stdout = StdoutRedirector(self.console_text)  
-    
-
+        sys.stdout = StdoutRedirector(self.console_text)
