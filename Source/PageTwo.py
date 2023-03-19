89009606ac0e5799a0a6212fa779dc1097f4540f
DESKTOP-QCSP4RS\m_mam <m_mammadli@hotmail.com>
2023-03-19 18:31:40 -0400

    pageTwo improvements

--- Source/PageTwo.py
+++ Source/PageTwo.py
@@ -1,33 +1,29 @@
-import os
 import customtkinter
-
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
-from matplotlib.figure import Figure
-from ByBit_MarketData import BTCUSD_MarketData
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+from ByBit_MarketData import BTCUSD_MarketData
+from matplotlib.figure import Figure
 
 # Use the 'ggplot' style sheet
 plt.style.use('seaborn-whitegrid')
 
-class BtcFigure():
-
-    f = Figure(figsize=(5,5), dpi=100)
-    axes = f.add_subplot(111)
+class BtcFigure:
+    f, axes = plt.subplots(figsize=(5, 5), dpi=100)
 
 def animate(i):
-    pullData = open("BTCUSD_f.txt","r").read()
-    dataList = pullData.split('\n')
-    xList = []
-    yList = []
-    for eachLine in dataList:
-        if len(eachLine) > 1:
-            x,y = eachLine.split(',')
-            xList.append(float(x))
-            yList.append(float(y))
-    BtcFigure.axes.clear
-    BtcFigure.axes.plot(xList,yList)
-    BtcFigure.axes.set_title('BTC-USD')
-    BtcFigure.axes.set_ylabel('USD')
+    with open("BTCUSD_f.txt", "r") as data_file:
+        data_list = data_file.read().split('\n')
+        x_list = []
+        y_list = []
+        for each_line in data_list:
+            if len(each_line) > 1:
+                x, y = each_line.split(',')
+                x_list.append(float(x))
+                y_list.append(float(y))
+        BtcFigure.axes.clear()
+        BtcFigure.axes.plot(x_list, y_list)
+        BtcFigure.axes.set_title('BTC-USD')
+        BtcFigure.axes.set_ylabel('USD')
 
 class PageTwo(customtkinter.CTkFrame):
 
@@ -36,11 +32,10 @@ class PageTwo(customtkinter.CTkFrame):
         self.controller = controller
         label = customtkinter.CTkLabel(self, text="Smart Trader", font=customtkinter.CTkFont(size=20, weight="bold"))
         label.pack(padx=20, pady=30)
-        button = customtkinter.CTkButton(self, text="Return",
-                           command=lambda: controller.show_frame("PageOne"))
+        button = customtkinter.CTkButton(self, text="Return", command=lambda: controller.show_frame("PageOne"))
         # Pack the button in the bottom-right corner of the window
         button.pack(side='bottom', anchor='se', padx=10, pady=10) 
-         
+
         canvas = FigureCanvasTkAgg(BtcFigure.f, self)
         canvas.draw()
         canvas.get_tk_widget().pack(side=customtkinter.TOP, fill=customtkinter.BOTH, expand=True)
@@ -48,8 +43,8 @@ class PageTwo(customtkinter.CTkFrame):
         toolbar = NavigationToolbar2Tk(canvas, self)
         toolbar.update()
         canvas._tkcanvas.pack(side=customtkinter.TOP, fill=customtkinter.BOTH, expand=True)
-        self.update_MarketData()
+        self.update_market_data()
         
-    def update_MarketData(self):
+    def update_market_data(self):
         BTCUSD_MarketData()
-        self.after(1000, self.update_MarketData)
+        self.after(1000, self.update_market_data)
